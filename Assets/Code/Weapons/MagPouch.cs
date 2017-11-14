1755b52bcf8bbeba292635e4f60c82f1ffda4d0f
Damien Ostler <damienostler1@outlook.com>
2017-11-14 17:23:31 -0500

    Setup Mag Pouches & Pistol Holster
    Created two scripts called MagPouch.cs WeaponHolster.cs, And setup the prefabs and put them in the weapons resources folder.

---
+++ Assets/Code/Weapons/MagPouch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+namespace VRWeapons.InteractionSystems.VRTK
+{
+
+    [RequireComponent(typeof(VRTK_SnapDropZone))]
+        
+    public class MagPouch : MonoBehaviour
+    {
+        Collider magCollider;
+        VRTK_SnapDropZone dropZone;
+
+        [Tooltip("If checked, this will disable the magazine's collider when it is inserted. Solves issues with grabbing magazines instead of the gun, in cases like pistols."), SerializeField]
+        bool disableColliderOnMagIn;
+
+        private void Start()
+        {
+            dropZone = GetComponent<VRTK_SnapDropZone>();
+            dropZone.ObjectSnappedToDropZone += new SnapDropZoneEventHandler(ObjectSnapped);
+            dropZone.ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(ObjectUnsnapped);
+        }
+
+        void ObjectSnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            magCollider = e.snappedObject.GetComponent<Collider>();
+              magCollider.isTrigger = true;
+        }
+
+        void ObjectUnsnapped(object sender, SnapDropZoneEventArgs e)
+        {
+            IMagazine mag = e.snappedObject.GetComponent<IMagazine>();
+            //Stop listening for mag drop event so we won't redundantly unsnap
+            magCollider.isTrigger = false;
+            magCollider = null;
+
+            //This is necessary for the initial mag so it won't revert to child of weapon
+            var interactable = e.snappedObject.GetComponent<VRTK_InteractableObject>();
+            if (interactable != null)
+            {
+                interactable.SaveCurrentState();
+            }
+
+        }
+
+    }
+}
---
+++ Assets/Code/Weapons/WeaponHolster.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+namespace VRWeapons.InteractionSystems.VRTK
+{
+
+	[RequireComponent(typeof(VRTK_SnapDropZone))]
+        
+	public class WeaponHolster : MonoBehaviour
+	{
+		VRWeapons.Weapon weapon;
+		VRTK_SnapDropZone dropZone;
+
+		[Tooltip("If checked, this will enable the second hand grip col to be disabled."), SerializeField]
+		bool disableSecondHandCol;
+
+		private void Start()
+		{
+			dropZone = GetComponent<VRTK_SnapDropZone>();
+			dropZone.ObjectSnappedToDropZone += new SnapDropZoneEventHandler(ObjectSnapped);
+			dropZone.ObjectUnsnappedFromDropZone += new SnapDropZoneEventHandler(ObjectUnsnapped);
+		}
+
+		void ObjectSnapped(object sender, SnapDropZoneEventArgs e)
+		{
+			weapon = e.snappedObject.GetComponent<VRWeapons.Weapon>();
+			weapon.weaponBodyCollider.isTrigger = true;
+			if (disableSecondHandCol)
+			{
+				weapon.secondHandGripCollider.isTrigger = true;
+			}
+		}
+
+		void ObjectUnsnapped(object sender, SnapDropZoneEventArgs e)
+		{
+			IMagazine mag = e.snappedObject.GetComponent<IMagazine>();
+			//Stop listening for mag drop event so we won't redundantly unsnap
+			weapon.weaponBodyCollider.isTrigger = false;
+			if (disableSecondHandCol)
+			{
+				weapon.secondHandGripCollider.isTrigger = false;
+			}
+
+			//This is necessary for the initial mag so it won't revert to child of weapon
+			var interactable = e.snappedObject.GetComponent<VRTK_InteractableObject>();
+			if (interactable != null)
+			{
+				interactable.SaveCurrentState();
+			}
+
+		}
+
+	}
+}
